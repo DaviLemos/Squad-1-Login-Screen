0d976b32efff82ef7bf92c562f34d7f218b81fb4
Paiva07 <paiva_lol@hotmail.com>
2021-11-19 14:02:15 -0300

    Corrigindo Erro do UseState na Feature

--- src/Screens/login.js
+++ src/Screens/login.js
@@ -1,60 +1,64 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Button, Text } from "../Components/Buttons/ButtonContinue";
 import { Form } from "../Components/Forms/Forms";
 import { InputPass, InputText, DivContainerText, DivContainerPass } from "../Components/Inputs/InputBase";
 import { User, Lock } from 'react-feather';
 import { Titulo } from "../Components/Titulo/Titulo";
-    
 
-    const [usuario, setUsuario] = useState("")
-    const [senha, setSenha] = useState("")
 
-function Login(){
+function Login() {
 
-    return(
-        <div>
+  const [usuario, setUsuario] = useState("")
+  const [senha, setSenha] = useState("")
 
-    <Form>
+  return (
+    <div>
 
-      <Titulo>Login</Titulo>
+      <Form>
 
-      <DivContainerText>
-        <InputText
-          placeholder="Usuário"
-          required
-          value={usuario}
+        <Titulo>Login</Titulo>
 
-          onChange={(event) => {
-            setUsuario(event.target.value);
-        }}
-        />
-        <User
-          color="#E0E0E0"
-          size={24}
-          style={{ marginTop: 'auto', marginBottom: 'auto' }}
-        />
-      </DivContainerText>
+        <DivContainerText>
+          <InputText
+            placeholder="Usuário"
+            required
+            value={usuario}
 
-      <DivContainerPass>
-        <InputPass
-          placeholder="Senha"
-          required
-          value={senha}
-        />
-        <Lock
-          color="#E0E0E0"
-          size={24}
-          style={{ marginTop: 'auto', marginBottom: 'auto' }}
-        />
-      </DivContainerPass>
-     
-      <Button>
-        <Text>Continuar</Text>
-      </Button>
+            onChange={(event) => {
+              setUsuario(event.target.value);
+            }}
+          />
+          <User
+            color="#E0E0E0"
+            size={24}
+            style={{ marginTop: 'auto', marginBottom: 'auto' }}
+          />
+        </DivContainerText>
 
-    </Form>
-  </div>
-    )
+        <DivContainerPass>
+          <InputPass
+            placeholder="Senha"
+            required
+            value={senha}
+
+            onChange={(event) => {
+              setSenha(event.target.value);
+            }}
+          />
+          <Lock
+            color="#E0E0E0"
+            size={24}
+            style={{ marginTop: 'auto', marginBottom: 'auto' }}
+          />
+        </DivContainerPass>
+
+        <Button>
+          <Text>Continuar</Text>
+        </Button>
+
+      </Form>
+    </div>
+  )
 }
 
 export default Login
